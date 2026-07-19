b035a4e29d8518a7132f4514b6967ce69780716f
Minh Quy <quy05.01.06@gmail.com>
2026-07-19 13:52:07 +0700

    lab 6

---
+++ lab6/src/main/java/vn/edu/gdu/lab6/exception/ResourceNotFoundException.java
@@ -0,0 +1,6 @@
+package vn.edu.gdu.lab6.exception;
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
